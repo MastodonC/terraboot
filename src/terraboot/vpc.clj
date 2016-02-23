7455f0ce777f027c4ae8ab36b9f9d8a3d2edd55f
thattommyhall <thattommyhall@gmail.com>
2016-02-23 00:28:34 +0000

    Indentation fixes

--- src/terraboot/vpc.clj
+++ src/terraboot/vpc.clj
@@ -47,12 +47,12 @@
     (apply merge-in
            (map
             (apply comp
-                       (map #(partial (fn [type resource] (update-in resource [:resource type] (fn [spec] (add-to-every-value-map spec :vpc_id vpc-id)))) %)
-                           ["aws_security_group"
-                            "aws_internet_gateway"
-                            "aws_subnet"
-                            "aws_route_table"]))
-                resources))))
+                   (map #(partial (fn [type resource] (update-in resource [:resource type] (fn [spec] (add-to-every-value-map spec :vpc_id vpc-id)))) %)
+                        ["aws_security_group"
+                         "aws_internet_gateway"
+                         "aws_subnet"
+                         "aws_route_table"]))
+            resources))))
 
 (defn vpn-user-data [vars]
   (cloud-config {:package_update true
@@ -94,107 +94,107 @@
 
 (def vpc-vpn-infra
   (merge-in
-            (resource "aws_vpc" vpc-name
-                      {:tags {:Name vpc-name}
-                       :cidr_block vpc-cidr-block})
+   (resource "aws_vpc" vpc-name
+             {:tags {:Name vpc-name}
+              :cidr_block vpc-cidr-block})
 
 
-            (in-vpc vpc-name
-                    (aws-instance "vpn" {
-                                         :user_data (vpn-user-data {:range-start (cidr-start vpc-cidr-block)
-                                                                    :fallback-dns (fallback-dns vpc-cidr-block)})
-                                         :subnet_id (id-of "aws_subnet" "public-b")
-                                         :ami "ami-bc5b48d0"
-                                         :vpc_security_group_ids [(id-of "aws_security_group" "vpn")
-                                                                  (id-of "aws_security_group" "allow_outbound")
-                                                                  ]
-                                         :associate_public_ip_address true
-                                         })
-                    (security-group "allow_outbound" {}
-                                    {:type "egress"
-                                     :from_port 0
-                                     :to_port 0
-                                     :protocol -1
-                                     :cidr_blocks [all-external]
-                                     })
+   (in-vpc vpc-name
+           (aws-instance "vpn" {
+                                :user_data (vpn-user-data {:range-start (cidr-start vpc-cidr-block)
+                                                           :fallback-dns (fallback-dns vpc-cidr-block)})
+                                :subnet_id (id-of "aws_subnet" "public-b")
+                                :ami "ami-bc5b48d0"
+                                :vpc_security_group_ids [(id-of "aws_security_group" "vpn")
+                                                         (id-of "aws_security_group" "allow_outbound")
+                                                         ]
+                                :associate_public_ip_address true
+                                })
+           (security-group "allow_outbound" {}
+                           {:type "egress"
+                            :from_port 0
+                            :to_port 0
+                            :protocol -1
+                            :cidr_blocks [all-external]
+                            })
 
-                    (security-group "allow_external_http_https" {}
-                                    {:from_port 80
-                                     :to_port 80
-                                     :cidr_blocks [all-external]
-                                     }
-                                    {:from_port 443
-                                     :to_port 443
-                                     :cidr_blocks [all-external]})
+           (security-group "allow_external_http_https" {}
+                           {:from_port 80
+                            :to_port 80
+                            :cidr_blocks [all-external]
+                            }
+                           {:from_port 443
+                            :to_port 443
+                            :cidr_blocks [all-external]})
 
-                    (security-group "vpn" {}
-                                    {:from_port 22
-                                     :to_port 22
-                                     :cidr_blocks [all-external]}
-                                    {:from_port 1194
-                                     :to_port 1194
-                                     :protocol "udp"
-                                     :cidr_blocks [all-external]})
+           (security-group "vpn" {}
+                           {:from_port 22
+                            :to_port 22
+                            :cidr_blocks [all-external]}
+                           {:from_port 1194
+                            :to_port 1194
+                            :protocol "udp"
+                            :cidr_blocks [all-external]})
 
-                    (security-group "allow-all-tcp-within-public-subnet" {}
-                                    {:from_port 0
-                                     :to_port 65535
-                                     :cidr_blocks (vals (:public cidr-block))})
+           (security-group "allow-all-tcp-within-public-subnet" {}
+                           {:from_port 0
+                            :to_port 65535
+                            :cidr_blocks (vals (:public cidr-block))})
 
-                    (security-group "allow-all-udp-within-public-subnet" {}
-                                    {:from_port 0
-                                     :to_port 65535
-                                     :protocol "udp"
-                                     :cidr_blocks (vals (:public cidr-block))})
+           (security-group "allow-all-udp-within-public-subnet" {}
+                           {:from_port 0
+                            :to_port 65535
+                            :protocol "udp"
+                            :cidr_blocks (vals (:public cidr-block))})
 
-                    (security-group "allow-icmp-within-public-subnet" {}
-                                    {:from_port 0
-                                     :to_port 65535
-                                     :protocol "udp"
-                                     :cidr_blocks (vals (:public cidr-block))})
+           (security-group "allow-icmp-within-public-subnet" {}
+                           {:from_port 0
+                            :to_port 65535
+                            :protocol "udp"
+                            :cidr_blocks (vals (:public cidr-block))})
 
-                    (resource "aws_internet_gateway" vpc-name
-                              {:tags {:Name "main"}})
+           (resource "aws_internet_gateway" vpc-name
+                     {:tags {:Name "main"}})
 
-                    (resource "aws_route_table" "public" {:tags { :Name "public"}
-                                                          :route { :cidr_block all-external
-                                                                  :gateway_id (id-of "aws_internet_gateway" vpc-name)}
-                                                          :vpc_id (id-of "aws_vpc" vpc-name)})
+           (resource "aws_route_table" "public" {:tags { :Name "public"}
+                                                 :route { :cidr_block all-external
+                                                         :gateway_id (id-of "aws_internet_gateway" vpc-name)}
+                                                 :vpc_id (id-of "aws_vpc" vpc-name)})
 
-                    ;; Public Subnets
-                    (resource-seq
-                     (apply concat
-                            (for [az azs]
-                              (let [subnet-name (stringify "public" "-" az)
-                                    nat-eip (stringify subnet-name "-nat")]
-                                [["aws_subnet" subnet-name {:tags {:Name subnet-name}
-                                                            :cidr_block (get-in cidr-block [:public az])
-                                                            :availability_zone (stringify region az)
-                                                            }]
-                                 ["aws_route_table_association" subnet-name {:route_table_id (id-of "aws_route_table" "public")
-                                                                             :subnet_id (id-of "aws_subnet" subnet-name)
-                                                                             }]
-                                 ["aws_nat_gateway" subnet-name {:allocation_id (id-of "aws_eip" nat-eip)
-                                                                 :subnet_id  (id-of "aws_subnet" subnet-name)}]
+           ;; Public Subnets
+           (resource-seq
+            (apply concat
+                   (for [az azs]
+                     (let [subnet-name (stringify "public" "-" az)
+                           nat-eip (stringify subnet-name "-nat")]
+                       [["aws_subnet" subnet-name {:tags {:Name subnet-name}
+                                                   :cidr_block (get-in cidr-block [:public az])
+                                                   :availability_zone (stringify region az)
+                                                   }]
+                        ["aws_route_table_association" subnet-name {:route_table_id (id-of "aws_route_table" "public")
+                                                                    :subnet_id (id-of "aws_subnet" subnet-name)
+                                                                    }]
+                        ["aws_nat_gateway" subnet-name {:allocation_id (id-of "aws_eip" nat-eip)
+                                                        :subnet_id  (id-of "aws_subnet" subnet-name)}]
 
-                                 ["aws_eip" nat-eip {:vpc true}]])
-                              )))
-                    ;; Private Subnets
+                        ["aws_eip" nat-eip {:vpc true}]])
+                     )))
+           ;; Private Subnets
 
-                    (resource-seq
-                     (apply concat
-                            (for [az azs]
-                              (let [subnet-name (stringify "private-" az)
-                                    public-subnet-name (stringify "public-" az)]
-                                [["aws_subnet" subnet-name {:tags {:Name subnet-name}
-                                                            :cidr_block (get-in cidr-block [:private az])
-                                                            :availability_zone (stringify region az)
-                                                            }]
-                                 ["aws_route_table" subnet-name {:tags {:Name subnet-name}
-                                                                 :route {:cidr_block all-external
-                                                                         :nat_gateway_id (id-of "aws_nat_gateway" public-subnet-name)}}
+           (resource-seq
+            (apply concat
+                   (for [az azs]
+                     (let [subnet-name (stringify "private-" az)
+                           public-subnet-name (stringify "public-" az)]
+                       [["aws_subnet" subnet-name {:tags {:Name subnet-name}
+                                                   :cidr_block (get-in cidr-block [:private az])
+                                                   :availability_zone (stringify region az)
+                                                   }]
+                        ["aws_route_table" subnet-name {:tags {:Name subnet-name}
+                                                        :route {:cidr_block all-external
+                                                                :nat_gateway_id (id-of "aws_nat_gateway" public-subnet-name)}}
 
-                                  ]
-                                 ["aws_route_table_association" subnet-name {:route_table_id (id-of "aws_route_table" subnet-name)
-                                                                             :subnet_id (id-of "aws_subnet" subnet-name)
-                                                                             }]])))))))
+                         ]
+                        ["aws_route_table_association" subnet-name {:route_table_id (id-of "aws_route_table" subnet-name)
+                                                                    :subnet_id (id-of "aws_subnet" subnet-name)
+                                                                    }]])))))))
