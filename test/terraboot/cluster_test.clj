b8c19ffa54a05c02be0584da0c3d78a165591dff
Elise Huard <elise@jabberwocky.eu>
2016-03-08 11:01:29 +0000

    tests fixed with interpolation

--- test/terraboot/cluster_test.clj
+++ test/terraboot/cluster_test.clj
@@ -7,17 +7,7 @@
 
 (def master-from-cloud-config
   (yaml/parse-string
-   (mesos-master-user-data {:aws-region "eu-central-1"
-                            :cluster-name "witan-production"
-                            :cluster-id "arn:aws:cloudformation:eu-central-1:165664414043:stack/witan-production/4faef210-d029-11e5-91a2-500c52a6cefe"
-                            :server-group "MasterServerGroup"
-                            :master-role "witan-production-MasterRole-BD3UQVJJBON"
-                            :slave-role "witan-production-SlaveRole-173MJXXXLJXYY"
-                            :aws-access-key "AKIAJT254WNI2YV7NBMA"
-                            :aws-secret-access-key "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"
-                            :exhibitor-s3-bucket "witan-production-exhibitors3bucket-ei8ratmz0ym7"
-                            :internal-lb-dns "internal-witan-pro-Internal-KIJPTA3IMQ1N-1673275547.eu-central-1.elb.amazonaws.com"
-                            :fallback-dns "10.64.0.2"})))
+   (mesos-master-user-data)))
 
 (def master-from-user-data
   (yaml/parse-string
@@ -38,17 +28,7 @@
 
 (def slave-from-cloud-config
   (yaml/parse-string
-   (mesos-slave-user-data {:aws-region "eu-central-1"
-                           :cluster-name "witan-production"
-                           :cluster-id "arn:aws:cloudformation:eu-central-1:165664414043:stack/witan-production/4faef210-d029-11e5-91a2-500c52a6cefe"
-                           :server-group "SlaveServerGroup"
-                           :master-role "witan-production-MasterRole-BD3UQVJJBON"
-                           :slave-role "witan-production-SlaveRole-173MJXXXLJXYY"
-                           :aws-access-key "AKIAJT254WNI2YV7NBMA"
-                           :aws-secret-access-key "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"
-                           :exhibitor-s3-bucket "witan-production-exhibitors3bucket-ei8ratmz0ym7"
-                           :internal-lb-dns "internal-witan-pro-Internal-KIJPTA3IMQ1N-1673275547.eu-central-1.elb.amazonaws.com"
-                           :fallback-dns "10.64.0.2"})))
+   (mesos-slave-user-data)))
 
 (expect (get-in slave-from-cloud-config [:coreos :units])
         (get-in slave-from-user-data [:coreos :units]))
@@ -63,17 +43,7 @@
 
 (def public-slave-from-cloud-config
   (yaml/parse-string
-   (mesos-public-slave-user-data {:aws-region "eu-central-1"
-                                  :cluster-name "witan-production"
-                                  :cluster-id "arn:aws:cloudformation:eu-central-1:165664414043:stack/witan-production/4faef210-d029-11e5-91a2-500c52a6cefe"
-                                  :server-group "PublicSlaveServerGroup"
-                                  :master-role "witan-production-MasterRole-BD3UQVJJBON"
-                                  :slave-role "witan-production-SlaveRole-173MJXXXLJXYY"
-                                  :aws-access-key "AKIAJT254WNI2YV7NBMA"
-                                  :aws-secret-access-key "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"
-                                  :exhibitor-s3-bucket "witan-production-exhibitors3bucket-ei8ratmz0ym7"
-                                  :internal-lb-dns "internal-witan-pro-Internal-KIJPTA3IMQ1N-1673275547.eu-central-1.elb.amazonaws.com"
-                                  :fallback-dns "10.64.0.2"})))
+   (mesos-public-slave-user-data)))
 
 (expect public-slave-from-cloud-config
         public-slave-from-user-data)
