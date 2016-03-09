0f31d9313eeddc760907a8fac0ddde700190eb8e
Elise Huard <elise@jabberwocky.eu>
2016-03-09 12:41:13 +0000

    escape possible interpolation

--- resources/system-files/backup-witan.sh
+++ resources/system-files/backup-witan.sh
@@ -9,11 +9,11 @@ AWS=$(find /opt/mesosphere -name aws)
 directory=$($NODETOOL snapshot witan | awk '/directory/{print $3}')
 
 # We assume it's worked if it tells us the directory
-if [ -z "${directory}" ]; then
+if [ -z "$${directory}" ]; then
    echo "Problem getting snapshot directory"
    exit 1;
 fi
 
 # We only want the snapshots dirs
 # For filters rationale see http://docs.aws.amazon.com/cli/latest/reference/s3/index.html#use-of-exclude-and-include-filters
-sudo -u core $AWS --profile backup s3 sync --exclude '*' --include '*/snapshots/*' ${CASSANDRA_DATA_DIR} s3://witan-cassandra-backup/$(hostname)/
+sudo -u core $AWS --profile backup s3 sync --exclude '*' --include '*/snapshots/*' $${CASSANDRA_DATA_DIR} s3://witan-cassandra-backup/$(hostname)/
