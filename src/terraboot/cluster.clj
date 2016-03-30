c65ba081ddb5cfaafea4ca62ef90763be127a1f6
Elise Huard <elise@jabberwocky.eu>
2016-03-30 18:09:27 +0200

    try to do without etcd2 because it doesn’t work

--- src/terraboot/cluster.clj
+++ src/terraboot/cluster.clj
@@ -61,13 +61,7 @@
 (defn mesos-master-user-data [etcd-token]
   (cloud-config (merge-with (comp vec concat)
                             (mesos-instance-user-data)
-                            { :coreos {:units {:name "etcd2.service" :command "start"}
-                                       :etcd2 {:discovery (str "https://discovery.etcd.io/" etcd-token)
-                                               :initial-advertise-peer-urls "http://$private_ipv4:2380"
-                                               :advertise-client-urls "http://$private_ipv4:2379,http://$private_ipv4:4001"
-                                               :listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001"
-                                               :listen-peer-urls "http://$private_ipv4:2380"}}
-                             :write_files [{:path "/etc/mesosphere/roles/master"
+                            {:write_files [{:path "/etc/mesosphere/roles/master"
                                             :content ""}
                                            {:path "/etc/mesosphere/roles/aws_master"
                                             :content ""}
