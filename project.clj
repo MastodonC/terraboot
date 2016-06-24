286822a06360501e536895a6829cf43901d53ff2
Elise Huard <elise@mastodonc.com>
2016-06-24 16:38:00 +0100

    changing terraboot into a library

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject terraboot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Terraform modules for kixi framework"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,4 @@
                  [cheshire "5.5.0"]
                  [stencil "0.5.0"]
                  [clj-yaml "0.4.0"]
-                 [expectations "2.0.9"]
-                 ]
-  :jvm-opts ["-Xmx2048m"]
-  :main terraboot.infra/-main)
+                 [expectations "2.0.9"]])
