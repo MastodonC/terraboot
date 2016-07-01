defc7e7598711398f7d5c97b4d94cb981c218323
Elise Huard <elise@mastodonc.com>
2016-07-01 12:31:57 +0100

    add kixi prefix for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject terraboot "0.1.0"
+(defproject kixi/terraboot "0.1.0"
   :description "Terraform modules for kixi framework"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
