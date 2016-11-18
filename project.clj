8c6600685575d2fe1f1fbe8f7c4aaeb430a03452
Elise Huard <elise@mastodonc.com>
2016-11-18 10:42:19 +0000

    add repositories for release

--- project.clj
+++ project.clj
@@ -7,4 +7,8 @@
                  [cheshire "5.5.0"]
                  [stencil "0.5.0"]
                  [clj-yaml "0.4.0"]
-                 [expectations "2.0.9"]])
+                 [expectations "2.0.9"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]])
