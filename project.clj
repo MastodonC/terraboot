668e7e808547cab3883026d3eba23b0e473e06e0
Elise Huard <elise@mastodonc.com>
2017-04-04 15:29:03 +0100

    Version 0.4.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/terraboot "0.4.9"
+(defproject kixi/terraboot "0.4.10-SNAPSHOT"
   :description "Terraform modules for kixi framework"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
