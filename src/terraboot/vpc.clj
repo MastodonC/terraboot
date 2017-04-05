033d76a5839c24bad9b1077de206ac69e54ceec2
Matt Ford <matt@dancingfrog.co.uk>
2017-04-05 14:31:36 +0100

    Remove influx and grafana entries

--- src/terraboot/vpc.clj
+++ src/terraboot/vpc.clj
@@ -118,44 +118,44 @@
                                                 :subnet_id (vpc-id-of "aws_subnet" (stringify "public-" (first azs)))
                                                 :ami default-ami
                                                 :vpc_security_group_ids [(vpc-id-of "aws_security_group" "vpn")
-                                                                         (vpc-id-of "aws_security_group" "sends_influx")
+                                                                         ;;(vpc-id-of "aws_security_group" "sends_influx")
                                                                          (vpc-id-of "aws_security_group" "all-servers")
                                                                          ]
                                                 :associate_public_ip_address true
                                                 })
 
-              (aws-instance "influxdb" {:ami default-ami
-                                        :instance_type "m4.large"
-                                        :vpc_security_group_ids [(vpc-id-of "aws_security_group" "influxdb")
-                                                                 (id-of "aws_security_group" "allow_ssh")
-                                                                 (vpc-id-of "aws_security_group" "allow_elb_grafana")
-                                                                 (vpc-id-of "aws_security_group" "all-servers")
-                                                                 ]
-                                        :subnet_id (vpc-id-of "aws_subnet" (stringify "public-" (first azs)))
-                                        :root_block_device {:volume_size 100}})
+              ;; (aws-instance "influxdb" {:ami default-ami
+              ;;                           :instance_type "m4.large"
+              ;;                           :vpc_security_group_ids [(vpc-id-of "aws_security_group" "influxdb")
+              ;;                                                    (id-of "aws_security_group" "allow_ssh")
+              ;;                                                    (vpc-id-of "aws_security_group" "allow_elb_grafana")
+              ;;                                                    (vpc-id-of "aws_security_group" "all-servers")
+              ;;                                                    ]
+              ;;                           :subnet_id (vpc-id-of "aws_subnet" (stringify "public-" (first azs)))
+              ;;                           :root_block_device {:volume_size 100}})
 
-              (elb "grafana" resource {:name "grafana"
-                                       :health_check {:healthy_threshold 2
-                                                      :unhealthy_threshold 3
-                                                      :target "HTTP:80/status"
-                                                      :timeout 5
-                                                      :interval 30}
-                                       :internal true
-                                       :listener [(elb-listener (if cert-name
-                                                                  {:lb-port 443 :lb-protocol "https" :port 80 :protocol "http" :cert-name cert-name}
-                                                                  {:port 80 :protocol "http"}))]
-                                       :instances [(id-of "aws_instance" "influxdb")]
-                                       :subnets (mapv #(id-of "aws_subnet" (stringify  vpc-name "-public-" %)) azs)
-                                       :security_groups (mapv #(id-of "aws_security_group" %) ["allow_outbound"
-                                                                                               "allow_external_http_https"
-                                                                                               (vpc-unique "elb_grafana")])
-                                       })
-              (private_route53_record "grafana" vpc-name {:zone_id  (vpc-id-of "aws_route53_zone" "mesos")
-                                                          :name "grafana"
-                                                          :alias {:name (output-of "aws_elb" "grafana" "dns_name")
-                                                                  :zone_id (output-of "aws_elb" "grafana" "zone_id")
-                                                                  :evaluate_target_health true}})
-              (private_route53_record "influxdb" vpc-name {:records [(output-of "aws_instance" "influxdb" "private_ip")]})
+              ;; (elb "grafana" resource {:name "grafana"
+              ;;                          :health_check {:healthy_threshold 2
+              ;;                                         :unhealthy_threshold 3
+              ;;                                         :target "HTTP:80/status"
+              ;;                                         :timeout 5
+              ;;                                         :interval 30}
+              ;;                          :internal true
+              ;;                          :listener [(elb-listener (if cert-name
+              ;;                                                     {:lb-port 443 :lb-protocol "https" :port 80 :protocol "http" :cert-name cert-name}
+              ;;                                                     {:port 80 :protocol "http"}))]
+              ;;                          :instances [(id-of "aws_instance" "influxdb")]
+              ;;                          :subnets (mapv #(id-of "aws_subnet" (stringify  vpc-name "-public-" %)) azs)
+              ;;                          :security_groups (mapv #(id-of "aws_security_group" %) ["allow_outbound"
+              ;;                                                                                  "allow_external_http_https"
+              ;;                                                                                  (vpc-unique "elb_grafana")])
+              ;;                          })
+              ;; (private_route53_record "grafana" vpc-name {:zone_id  (vpc-id-of "aws_route53_zone" "mesos")
+              ;;                                             :name "grafana"
+              ;;                                             :alias {:name (output-of "aws_elb" "grafana" "dns_name")
+              ;;                                                     :zone_id (output-of "aws_elb" "grafana" "zone_id")
+              ;;                                                     :evaluate_target_health true}})
+              ;; (private_route53_record "influxdb" vpc-name {:records [(output-of "aws_instance" "influxdb" "private_ip")]})
 
               (private_route53_record "kibana" vpc-name {:records [(vpc-output-of "aws_instance" "logstash" "private_ip")]})
               (private_route53_record "logstash" vpc-name {:records [(vpc-output-of "aws_instance" "logstash" "private_ip")]})
@@ -177,9 +177,9 @@
 
               (vpc-security-group "sends_influx" {})
 
-              (resource "aws_eip" "influxdb"
-                        {:vpc true
-                         :instance (id-of "aws_instance" "influxdb")})
+              ;; (resource "aws_eip" "influxdb"
+              ;;           {:vpc true
+              ;;            :instance (id-of "aws_instance" "influxdb")})
 
               (vpc-resource "aws_eip" "vpn" {:instance (vpc-id-of "aws_instance" "vpn")
                                              :vpc true})
