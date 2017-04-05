cec6b1dfd63aa0c50c72a12d2cd40651bff46bc9
Elise Huard <elise@mastodonc.com>
2017-04-05 14:36:33 +0100

    public dns refactored

--- src/terraboot/public_dns.clj
+++ src/terraboot/public_dns.clj
@@ -1,9 +1,10 @@
 (ns terraboot.public-dns
   (:require [terraboot.core :as core]
-            [terraboot.utils :as utils]))
+            [terraboot.utils :as utils]
+            [clojure.string :as string :refer [join replace]]))
 
-(defn public-route53-record [dns-zone dns-zone-id prefix spec]
-  (let [name (str prefix "." dns-zone)]
+(defn public-route53-record [root-dns dns-zone-id prefix spec]
+  (let [name (str prefix "." root-dns)]
     (core/resource "aws_route53_record" (core/safe-name name)
                    (merge
                     {:zone_id dns-zone-id
@@ -12,9 +13,24 @@
                     (if (:alias spec) {} {:ttl "300"})
                     spec))))
 
+(defn public-dns-zone
+  [environment-dns environment-dns-identifier]
+  (core/resource "aws_route53_zone" environment-dns-identifier
+                 {:name environment-dns
+                  :comment (str "public routes for " environment-dns)}))
+
 (defn vpc-public-dns
-  [dns-zone dns-zone-id vpc-name]
-  (let [route53-record (partial public-route53-record dns-zone dns-zone-id)
-        vpc-output-of (core/output-of-fn (core/vpc-unique-fn vpc-name))]
+  [{:keys [root-dns root-dns-zone-id environment project vpc-name]}]
+  (let [environment-dns (string/join "." [environment project root-dns])
+        environment-dns-identifier (string/replace environment-dns #"\." "_")]
     (utils/merge-in
-     (route53-record "vpn" {:records [(vpc-output-of "aws_eip" "vpn" "public_ip")]}))))
+     (public-dns-zone environment-dns environment-dns-identifier)
+     (public-route53-record root-dns
+                            root-dns-zone-id
+                            (string/join "." [environment project])
+                            {:type "NS"
+                             :records (mapv #(core/output-of "aws_route53_zone" environment-dns-identifier (string/join  "." ["name_server" %])) (range 0 4))})
+     (public-route53-record environment-dns
+                            (core/id-of "aws_route53_zone" environment-dns-identifier)
+                            "vpn"
+                            {:records [(core/output-of "aws_eip" (str vpc-name "-vpn") "public_ip")]}))))
