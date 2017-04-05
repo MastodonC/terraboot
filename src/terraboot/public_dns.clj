66fe5f6a78f03ccadbfa36b85fda3c2d1a6243e9
Elise Huard <elise@mastodonc.com>
2017-04-05 10:57:14 +0100

    remove unused dns

--- src/terraboot/public_dns.clj
+++ src/terraboot/public_dns.clj
@@ -17,23 +17,4 @@
   (let [route53-record (partial public-route53-record dns-zone dns-zone-id)
         vpc-output-of (core/output-of-fn (core/vpc-unique-fn vpc-name))]
     (utils/merge-in
-     (route53-record "vpn" {:records [(vpc-output-of "aws_eip" "vpn" "public_ip")]})
-     (route53-record "logstash" {:records [(vpc-output-of "aws_eip" "logstash" "public_ip")]})
-     (route53-record "alerts" {:type "CNAME"
-                               :records [(core/output-of "aws_elb" "alerts" "dns_name")]}))))
-
-(defn cluster-public-dns
-  [dns-zone dns-zone-id vpc-name cluster-name]
-  (let [route53-record (partial public-route53-record dns-zone dns-zone-id)
-        cluster-identifier (core/cluster-identifier vpc-name cluster-name)
-        cluster-unique (core/cluster-unique-fn vpc-name cluster-name)
-        cluster-output-of (core/output-of-fn cluster-unique)]
-    (utils/merge-in
-     (route53-record (cluster-unique "deploy")
-                     {:alias {:name (cluster-output-of "aws_alb" "public-apps" "dns_name")
-                              :zone_id (cluster-output-of "aws_alb" "public-apps" "zone_id")
-                              :evaluate_target_health true}})
-     (route53-record cluster-identifier
-                     {:alias {:name (cluster-output-of "aws_alb" "public-apps" "dns_name")
-                              :zone_id (cluster-output-of "aws_alb" "public-apps" "zone_id")
-                              :evaluate_target_health true}}))))
+     (route53-record "vpn" {:records [(vpc-output-of "aws_eip" "vpn" "public_ip")]}))))
