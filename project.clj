f260f212328b7a1d6506b50843cd983ecf38d648
Matt Ford <matt@dancingfrog.co.uk>
2017-04-24 10:58:27 +0100

    Version 0.4.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/terraboot "0.4.15-SNAPSHOT"
+(defproject kixi/terraboot "0.4.15"
   :description "Terraform modules for kixi framework"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
