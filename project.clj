cded9339c7f9fe6f4cca6dd371e50b48846b8dff
Tom Coupland <tcoupland@gmail.com>
2017-07-05 11:39:51 +0100

    Version 0.4.17-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/terraboot "0.4.16"
+(defproject kixi/terraboot "0.4.17-SNAPSHOT"
   :description "Terraform modules for kixi framework"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
