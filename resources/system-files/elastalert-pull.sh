e9dc1e7eaa24b92aa3bec37ca8a1d8eb9732d8ef
Matt Ford <matt@dancingfrog.co.uk>
2017-12-11 19:43:39 +0000

    Terraform can't cope with this syntax in template files.

--- resources/system-files/elastalert-pull.sh
+++ resources/system-files/elastalert-pull.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -x
 # Parse Args
-REPO=${1:?Error: provide the Github repo as the first argument}
-KEY=${2:?Error: provide the Github deploy key path as the second argument}
-DIR=${3:?Error: provide the dir to use for the rules location as the third argument}
+REPO=${1}
+KEY=${2}
+DIR=${3}
 
 pushd () {
     command pushd "$@" > /dev/null
@@ -16,8 +15,6 @@ popd () {
 # Init
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $KEY"
-
-#- Create Dir
 if [[ ! -d /opt/elastalert/src/rules ]] ; then
   mkdir -p /opt/elastalert/src
   pushd /opt/elastalert/src
